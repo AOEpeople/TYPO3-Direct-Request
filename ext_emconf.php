b0b9e4234a678ed70103208ed1cffe088e5e6214
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-05 11:23:06 +0000

    Raised version number to 0.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/directrequest/trunk@32953 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,4 +1,15 @@
 <?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "directrequest".
+#
+# Auto generated 05-05-2010 13:22
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Direct Request',
 	'description' => 'Extension the fake an HTTP request and perform a direct request to the frontend triggered by the backend',
@@ -10,24 +21,24 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'AOE media GmbH',
-	'version' => '0.0.dev',
+	'version' => '0.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.3.0-',
+			'typo3' => '4.3.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => '',
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"bd3d";s:16:"ext_autoload.php";s:4:"e869";s:21:"ext_conf_template.txt";s:4:"d67d";s:17:"ext_localconf.php";s:4:"ae1f";s:14:"ext_tables.php";s:4:"9c67";s:42:"classes/class.tx_directrequest_manager.php";s:4:"f92b";s:15:"cli/request.php";s:4:"0bdb";}',
 	'suggests' => array(
 	),
 );
