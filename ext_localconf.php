679ef734795587bf24b24e78fe22f31ea7b01494
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-12 16:24:56 +0000

    Fixed bug: Wrong definitions in ext_localconf.php file and ext_tables.php is superfluous
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/directrequest/trunk@33148 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,4 @@
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
-
-$TYPO3_CONF_VARS['EXTCONF']['tx_feuserregister']['addObserver'][] = 'EXT:' . $_EXTKEY . 'observers/class.tx_pagemailerfeuserregisterconnector_observer.php:tx_pagemailerfeuserregisterconnector_observer';
 ?>
