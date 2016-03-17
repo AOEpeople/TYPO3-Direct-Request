4f6613ad9cdf430576eaa5059b1ce2be238d9ae1
michael.sandritter <michael.sandritter@aoe.com>
2016-03-17 15:18:48 +0100

    added composer.json and connected repository with packagist. changed dependency constraint of typo3 from 4.3.0 to 4.3.0-6.2.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.9',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.3.0-0.0.0',
+			'typo3' => '4.3.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
